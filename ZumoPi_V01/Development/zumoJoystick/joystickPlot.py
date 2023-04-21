040b9c48e1b9d45092df856b85382593b4e31eb6
Arkadiraf <arkadiraf@gmail.com>
2023-04-21 15:59:52 +0300

    Update joystickPlot.py

--- ZumoPi_V01/Development/zumoJoystick/joystickPlot.py
+++ ZumoPi_V01/Development/zumoJoystick/joystickPlot.py
@@ -13,7 +13,6 @@ import dash_daq as daq
 from dash import html
 import dash_core_components as dcc
 import plotly.graph_objs as go
-import logging
 
 # ZumoController class
 class ZumoController:
@@ -22,10 +21,9 @@ class ZumoController:
         self.zumo_angle = 0
         self.zumo_speed = 0
         self.closing_event = threading.Event()
-
         self.transmit_thread = threading.Thread(target=self.transmit_data)
         self.receive_thread = threading.Thread(target=self.receive_data)
-
+        self.lock = threading.Lock()
         # Open a file to save the incoming messages
         self.incoming_messages_file = open('incoming_messages.txt', 'a')
         self.last_messages = []
@@ -42,11 +40,12 @@ class ZumoController:
 
     def transmit_data(self):
         while not self.closing_event.is_set() and self.ser.isOpen:
-            self.zumo_angle = self.zumo_angle if self.zumo_angle is not None else 0
-            self.zumo_speed = self.zumo_speed if self.zumo_speed is not None else 0
+            with self.lock:
+                zumo_angle = self.zumo_angle if self.zumo_angle is not None else 0
+                zumo_speed = self.zumo_speed if self.zumo_speed is not None else 0
 
-            joy_y = math.sin(math.radians(self.zumo_angle)) * self.zumo_speed * 200
-            joy_x = math.cos(math.radians(self.zumo_angle)) * self.zumo_speed * 200
+            joy_y = math.sin(math.radians(zumo_angle)) * zumo_speed * 200
+            joy_x = math.cos(math.radians(zumo_angle)) * zumo_speed * 200
 
             msg = f'{joy_x},{joy_y}\r\n'
             self.ser.write(msg.encode('ascii'))
@@ -56,10 +55,11 @@ class ZumoController:
         while not self.closing_event.is_set() and self.ser.isOpen:
             if self.ser.in_waiting > 0:
                 c = self.ser.readline().decode("ascii")
-                self.incoming_messages_file.write(c)
-                self.last_messages.append(c.strip())
-                if len(self.last_messages) > 5:
-                    self.last_messages.pop(0)
+                with self.lock:
+                    self.incoming_messages_file.write(c)
+                    self.last_messages.append(c.strip())
+                    if len(self.last_messages) > 5:
+                        self.last_messages.pop(0)
                 print(c)
             else:
                 time.sleep(0.05)
@@ -69,13 +69,23 @@ class ZumoController:
         self.close()
         dash_app_thread.join()
 
+    def set_zumo_angle(self, angle):
+        self.zumo_angle = angle if isinstance(angle, (int, float)) else 0
+
+    def set_zumo_speed(self, speed):
+        self.zumo_speed = speed if isinstance(speed, (int, float)) else 0
+
+    def get_last_messages(self):
+        return self.last_messages
+
     def close(self):
         self.closing_event.set()
         self.incoming_messages_file.close()
 
 # ZumoDashApp class
 class ZumoDashApp:
-    def __init__(self, external_stylesheets):
+    def __init__(self, external_stylesheets, zumo_controller):
+        self.zumo_controller = zumo_controller
         self.received_values = deque(maxlen=500)
         self.app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
         self.create_layout()
@@ -99,23 +109,26 @@ class ZumoDashApp:
         ])
 
     def update_output(self, angle, force):
-        global zumo_controller
-        zumo_controller.zumo_angle = angle if isinstance(angle, (int, float)) else 0
-        zumo_controller.zumo_speed = force if isinstance(force, (int, float)) else 0
+        with self.zumo_controller.lock:
+            self.zumo_controller.set_zumo_angle(angle)
+            self.zumo_controller.set_zumo_speed(force)
 
         return [f'Angle is {angle}',
                 html.Br(),
                 f'Force is {force}']
 
     def update_incoming_messages(self, _):
-        global zumo_controller
-        return html.Div([html.P(message) for message in zumo_controller.last_messages])
+        with self.zumo_controller.lock:
+            last_messages = self.zumo_controller.get_last_messages()
+        return html.Div([html.P(message) for message in last_messages])
+
 
     def update_plot(self, n_intervals):
-        global zumo_controller
+        with self.zumo_controller.lock:
+            last_messages = self.zumo_controller.get_last_messages()
 
-        if zumo_controller.last_messages:
-            values = zumo_controller.last_messages[-1].split(',')
+        if last_messages:
+            values = last_messages[-1].split(',')
             values = list(map(float, values))
             self.received_values.append(values)
 
@@ -155,7 +168,7 @@ class ZumoDashApp:
 
 def main():
     with ZumoController('/dev/ttyACM0') as zumo_controller:
-        zumo_dash_app = ZumoDashApp(['pydash\templates\bWLwgP.css'])
+        zumo_dash_app = ZumoDashApp(['pydash\templates\bWLwgP.css'], zumo_controller)
         zumo_controller.start()
 
         dash_app_thread = threading.Thread(target=zumo_dash_app.start)
